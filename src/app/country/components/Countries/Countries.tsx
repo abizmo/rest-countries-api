7d7f1af7b1053a13102e89d91fcc99dfef0fc38c
Abián Izquierdo <abizmo@gmail.com>
2022-10-03 18:36:27 +0100

    Add styles to countries view

--- src/app/country/components/Countries/Countries.tsx
+++ src/app/country/components/Countries/Countries.tsx
@@ -26,35 +26,35 @@ export const Countries: React.FC = () => {
   return (
     <ul className={styles.container} role='list'>
       {countries.map((country) => (
-        <li key={country.code}>
-          <div className={styles.card}>
-            <Link to={`country/${country.code}`}>
+        <li key={country.code} className={styles.card}>
+          <Link to={`country/${country.code}`}>
+            <div className={styles.cardHeader}>
               <img
                 src={country.flag}
                 alt={country.name}
                 className={styles.cardImage}
               />
-              <div className={styles.cardBody}>
-                <h3 className={`${styles.cardTitle} | bold fs-600`}>
-                  {country.name}
-                </h3>
-                <ul className={styles.cardDetails} role='list'>
-                  <li className='fs-400'>
-                    <span className='medium'>Population: </span>
-                    {country.population.toLocaleString()}
-                  </li>
-                  <li>
-                    <span className='medium'>Region: </span>
-                    {country.region}
-                  </li>
-                  <li>
-                    <span className='medium'>Capital: </span>
-                    {country.capital}
-                  </li>
-                </ul>
-              </div>
-            </Link>
-          </div>
+            </div>
+            <div className={styles.cardBody}>
+              <h3 className={`${styles.cardTitle} | bold fs-600`}>
+                {country.name}
+              </h3>
+              <ul className={`${styles.cardDetails} | fs-400`} role='list'>
+                <li>
+                  <span className='medium'>Population: </span>
+                  {country.population.toLocaleString()}
+                </li>
+                <li>
+                  <span className='medium'>Region: </span>
+                  {country.region}
+                </li>
+                <li>
+                  <span className='medium'>Capital: </span>
+                  {country.capital}
+                </li>
+              </ul>
+            </div>
+          </Link>
         </li>
       ))}
     </ul>
