568e00325b81040be667e3c1feaf410844451af7
Abián Izquierdo <abizmo@gmail.com>
2022-10-21 17:52:23 +0100

    Fix display borders Name

--- src/app/country/infrastructure/country.repository.ts
+++ src/app/country/infrastructure/country.repository.ts
@@ -1,51 +1,72 @@
 import { CountryRepository } from '../domain/country.repository';
 import { Http } from '../../http/domain/http.repository';
 import { CountryDTO } from './country.dto';
+import { Border } from '../domain/country.model';
 
 const BASE_URL = 'https://restcountries.com/v3.1';
 
-export const countryRepository = (client: Http): CountryRepository => ({
-  getCountries: async () => {
+export const countryRepository = (client: Http): CountryRepository => {
+  const getBorders = async (borderCodes: string[]): Promise<Border[]> => {
     try {
-      const countries = await client.get<CountryDTO[]>(`${BASE_URL}/all`);
-
-      return countries.map(
-        ({ flags, name, population, region, capital, cca3 }: CountryDTO) => ({
-          code: cca3,
-          flag: flags.png,
-          name: name.common,
-          population,
-          region,
-          capital: capital?.[0],
-        }),
+      const borderCountries: CountryDTO[] = await client.get<CountryDTO[]>(
+        `${BASE_URL}/alpha?codes=${borderCodes.join(',')}`,
       );
+
+      return borderCountries.map(({ name, cca3 }, idx) => ({
+        name: name.common,
+        code: cca3,
+      }));
     } catch (err) {
       console.error(err);
       return [];
     }
-  },
-  getCountryByCode: async (code) => {
-    try {
-      const countries: CountryDTO[] = await client.get<CountryDTO[]>(
-        `${BASE_URL}/alpha/${code}`,
-      );
+  };
 
-      return countries.map((country: CountryDTO) => ({
-        code: country.cca3,
-        flag: country.flags.png,
-        name: country.name.common,
-        population: country.population,
-        region: country.region,
-        capital: country.capital?.[0],
-        nativeName: Object.values(country.name.nativeName)[0].common,
-        subregion: country.subregion,
-        tld: country.tld,
-        currencies: Object.values(country.currencies)[0].name,
-        languages: Object.values(country.languages),
-        borders: country.borders,
-      }))[0];
-    } catch (err) {
-      console.error(err);
-    }
-  },
-});
+  return {
+    getCountries: async () => {
+      try {
+        const countries = await client.get<CountryDTO[]>(`${BASE_URL}/all`);
+
+        return countries.map(
+          ({ flags, name, population, region, capital, cca3 }: CountryDTO) => ({
+            code: cca3,
+            flag: flags.png,
+            name: name.common,
+            population,
+            region,
+            capital: capital?.[0],
+          }),
+        );
+      } catch (err) {
+        console.error(err);
+        return [];
+      }
+    },
+    getCountryByCode: async (code) => {
+      try {
+        const [country]: CountryDTO[] = await client.get<CountryDTO[]>(
+          `${BASE_URL}/alpha/${code}`,
+        );
+
+        const borders = await getBorders(country.borders);
+
+        return {
+          code: country.cca3,
+          flag: country.flags.png,
+          name: country.name.common,
+          population: country.population,
+          region: country.region,
+          capital: country.capital?.[0],
+          nativeName: Object.values(country.name.nativeName)[0].common,
+          subregion: country.subregion,
+          tld: country.tld,
+          currencies: Object.values(country.currencies)[0].name,
+          languages: Object.values(country.languages),
+          borders,
+        };
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  };
+};
